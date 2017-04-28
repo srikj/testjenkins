61b20ab759b4a3604f2c96eaa29ae30d0107ad04
Srikanth J <srikj@cisco.com>
2017-04-28 15:19:16 +0530

    Initial Git commit

---
+++ src/test/java/com/cisco/test/jenkinsTest/AppTest.java
@@ -0,0 +1,30 @@
+package com.cisco.test.jenkinsTest;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+	
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+	  System.out.println("method name:" + result.getMethod().getMethodName()+"result = "+(result.isSuccess()?"Pass":"Fail"));
+	}
+	
+	@Test
+	public void passingTest() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void failingTest() {
+		Assert.assertTrue(false);
+	}
+    
+}
---
+++ src/test/java/com/cisco/test/jenkinsTest/SampleTest.java
@@ -0,0 +1,48 @@
+package com.cisco.test.jenkinsTest;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SampleTest 
+{
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+	  System.out.println("method name:" + result.getMethod().getMethodName()+"result = "+(result.isSuccess()?"Pass":"Fail"));
+	}
+	
+	@Test
+	public void passingTest() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void passingTest1() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void passingTest2() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void failingTest() {
+		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void failingTest1() {
+		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void failingTest2() {
+		Assert.assertTrue(false);
+	}
+    
+}
